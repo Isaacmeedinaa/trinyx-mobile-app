8a87caae59190f27c50c29f8652c29b055284e09
Isaac Medina <isaac@Isaacs-Air.hsd1.il.comcast.net>
2020-06-24 21:52:57 -0500

    first commit and basic app screens

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import BusinessLoginScreen from "./app/screens/BusinessLoginScreen";
+import UserLoginScreen from "./app/screens/UserLoginScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome" screenOptions={{ title: "" }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="UserLogin" component={UserLoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/config/colors.js
@@ -0,0 +1,5 @@
+export default {
+  primary: "#de3d4d",
+  light: "#f8f8f8",
+  dark: "#000",
+};
